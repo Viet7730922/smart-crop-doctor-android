5fdaf01142868be50a3983fc880364e3741d01ec
Viet7730922 <voqviet7206@gmail.com>
2026-05-27 08:38:19 +0700

    Update Alert Icon.

--- app/src/main/java/ntu/viet773092/ungDungCdbct_65134318/adapter/HistoryAdapter.java
+++ app/src/main/java/ntu/viet773092/ungDungCdbct_65134318/adapter/HistoryAdapter.java
@@ -76,7 +76,7 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryV
             new MaterialAlertDialogBuilder(context)
                     .setTitle("Xóa nhật ký chẩn đoán?")
                     .setMessage("Hành động này sẽ xóa vĩnh viễn phác đồ và hình ảnh bệnh phẩm này khỏi bộ nhớ thiết bị. Bạn có chắc chắn không?")
-                    .setIcon(R.drawable.ic_attention)
+                    .setIcon(R.drawable.ic_alert_2)
                     .setCancelable(true)
                     .setPositiveButton("Xóa dữ liệu", (dialog, which) -> {
                         // Khởi tạo luồng xóa SQLite cục bộ
